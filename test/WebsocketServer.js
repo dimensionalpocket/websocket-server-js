e614fa3e8a6339706b88e76a8f824a5438cf7b19
pauldps <github@pauloddr.com>
2023-02-12 17:19:33 -0500

    more specs

--- test/WebsocketServer.js
+++ test/WebsocketServer.js
@@ -1,8 +1,41 @@
 import { expect, sinon } from '@dimensionalpocket/development'
 import { WebsocketServer } from '../src/WebsocketServer.js'
+import { createServer } from './utils/create-server.js'
 
 describe('WebsocketServer', function () {
   describe('#stop', function () {
+    context('when server is started', function () {
+      before(function (done) {
+        sinon.spy(console, 'log')
+
+        createServer(this, (/** @type {any} */ err) => {
+          if (err) return done(err)
+
+          sinon.stub(this.server, 'emit')
+          this.server.stop()
+          done()
+        })
+      })
+
+      after(function () {
+        // @ts-ignore
+        console.log.restore()
+        this.server.emit.restore()
+      })
+
+      it('emits stop event', function () {
+        expect(this.server.emit).to.have.been.calledWith('stop', this.server)
+      })
+
+      it('logs that the server has stopped', function () {
+        expect(console.log).to.have.been.calledWith('Server', this.server.uuid, 'stopped.')
+      })
+
+      it('unsets the internal listen socket', function () {
+        expect(this.server._uwsSocket).to.eq(null)
+      })
+    })
+
     context('when the server is not started', function () {
       before(function () {
         sinon.stub(console, 'log')
@@ -19,7 +52,7 @@ describe('WebsocketServer', function () {
         expect(this.result).to.eq(false)
       })
 
-      it('logs that the server is stopped', function () {
+      it('logs that the server is already stopped', function () {
         expect(console.log).to.have.been.calledWith('Server', this.server.uuid, 'already stopped.')
       })
     })
